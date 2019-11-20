2748dabb70b993964bb910eb4e5ff8134c98d306
Marcelo Vinicius <marcelo_vinicius7@hotmail.com>
2019-11-20 12:27:30 -0300

    Progresso service criado

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -1,7 +1,34 @@
 import * as firebase from 'firebase'
+import { Injectable } from '@angular/core'
+import { Progresso } from './progresso.service'
 
+@Injectable()
 export class Bd {
+
+    constructor(private progresso: Progresso) {}
+
     public publicar(publicacao: any): void {
+
+        let nomeImagem = Date.now()
+
+        firebase.storage().ref()
+        .child(`imagens/${nomeImagem}`)
+        .put(publicacao.imagem)
+        .on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: any) => {
+            this.progresso.status = 'andamento'
+            this.progresso.estado = snapshot
+            //console.log(snapshot)
+        }, 
+        (error) => {
+            this.progresso.status = 'erro'
+            //console.log(error)
+        }, 
+        () => {
+            this.progresso.status = 'concluido'
+           // console.log("upload completo")
+        }
+        )
         
         firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
         .push( { titulo: publicacao.titulo } )
--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms'
 import { Bd } from '../../bd.service'
+import { Progresso } from '../../progresso.service'
+
 import * as firebase from 'firebase'
 
 @Component({
@@ -11,12 +13,13 @@ import * as firebase from 'firebase'
 export class IncluirPublicacaoComponent implements OnInit {
 
   public email: string
+  private imagem: any
 
   public formulario: FormGroup = new FormGroup({
     'titulo': new FormControl(null)
   })
 
-  constructor(private bd: Bd) { }
+  constructor(private bd: Bd, private progresso: Progresso) { }
 
   ngOnInit() {
     firebase.auth().onAuthStateChanged((user) => {
@@ -27,8 +30,16 @@ export class IncluirPublicacaoComponent implements OnInit {
   public publicar(): void {
     this.bd.publicar({
       email: this.email,
-      titulo: this.formulario.value.titulo
+      titulo: this.formulario.value.titulo,
+      imagem: this.imagem[0]
     })
+
+    //this.progresso.status
+    //this.progresso.estado
+  }
+
+  public preparaImagemUpload(event: Event): void {
+    this.imagem = (<HTMLInputElement>event.target).files
   }
 
 }
