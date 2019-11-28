e0e6571ad1bb235b359c517a3f94c546b3a0ea07
Marcelo Vinicius <marcelo_vinicius7@hotmail.com>
2019-11-28 16:14:50 -0300

    Listando publicacoes

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -35,4 +35,43 @@ export class Bd {
 
             })
     }
+
+    public consultarPublicacoes(emailUsuario: string): Promise<any> {
+
+        return new Promise((resolve, reject) => {
+
+            //consultar as publicações (database)
+            firebase.database().ref(`publicacoes/${btoa(emailUsuario)}`)
+                .once('value')
+                .then((snapshot: any) => {
+
+                    let publicacoes: Array<any> = [];
+
+                    snapshot.forEach((childSnapshop: any) => {
+
+                        let publicacao = childSnapshop.val()
+
+                        //consultar a url da imagem(storege)
+                        firebase.storage().ref()
+                            .child(`imagens/${childSnapshop.key}`)
+                            .getDownloadURL()
+                            .then((url: string) => {
+                                publicacao.url_imagem = url
+
+                                //consultar o nome do usuario
+                                firebase.database().ref(`usuario_detalhe/${btoa(emailUsuario)}`)
+                                    .once('value')
+                                    .then((snapshot: any) => {
+                                        publicacao.nome_usuario = snapshot.val().nome_usuario
+
+                                        publicacoes.push(publicacao)
+                                    })
+
+                            })
+                    })
+
+                    resolve(publicacoes)
+                })
+        })
+    }
 }
